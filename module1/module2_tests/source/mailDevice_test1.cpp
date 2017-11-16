33e3ab7fbe7769d92222e354ba31efe84987a6fa
Piotr Gorecki <piotr.gorecki@mobica.com>
2017-11-16 16:33:54 +0100

    szkolenie1

--- module1/module2_tests/source/mailDevice_test1.cpp
+++ module1/module2_tests/source/mailDevice_test1.cpp
@@ -27,5 +27,13 @@ protected:
 #ifndef IntegrationTests
 TEST_F(mailDevice_test1, openDriverSuccess)
 {
+	EXPECT_FALSE(device->openDriver() == NULL);
 }
+
+TEST_F(mailDevice_test1, closeDriverSuccess)
+{
+	FILE* id = device->openDriver();
+	EXPECT_EQ(0, device->closeDriver(id));
+}
+
 #endif
